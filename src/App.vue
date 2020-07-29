7e01afee523d0d47edcd50042ed18e32eb624fa2
Stephen <stephen_h369@live.co.uk>
2020-07-29 15:31:32 +0100

    initial app.vue setup, created component files

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Countries</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      countries: [],
+      selectedCountry: null
+    };
+  },
+  mounted() {
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(response => response.json())
+    .then(countries => this.countries = countries)
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
